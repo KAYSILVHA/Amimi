0c9884bdf2756666b5664677b51bc1bb9a7594a3
KAYSILVHA <kaysilvha@gmail.com>
2023-06-15 01:14:13 -0300

    testes

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+const btnMobilie = document.getElementById('btn-mobile');
+
+function toggleMenu(){
+  const btnMobilie = document.getElementById('nav');
+  nav.classList.toggle('active');
+}
+
+btnMobile.addEventListener('click', toggleMenu);
